5d9fb09c562ccf4b48b4188ad899e8f1c6b604b0
asl0044 <asl0044@alu.ubu.es>
2016-05-05 18:45:34 +0200

    Versión inicial

---
+++ Practica2ChatRMI/src/main/java/Practica2ChatRMI/Practica2ChatRMI/App.java
@@ -0,0 +1,13 @@
+package Practica2ChatRMI.Practica2ChatRMI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
