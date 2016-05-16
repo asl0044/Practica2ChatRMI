726effb054f03e601b4dd265f75c7e46ebcb03fb
asl0044 <asl0044@alu.ubu.es>
2016-05-16 18:40:30 +0200

    Implementado ChatServerStarter

--- Practica2ChatRMI/src/main/java/es/ubu/lsi/server/ChatServerStarter.java
+++ Practica2ChatRMI/src/main/java/es/ubu/lsi/server/ChatServerStarter.java
@@ -1,5 +1,28 @@
 package es.ubu.lsi.server;
 
-public class ChatServerStarter {
+import java.rmi.Naming;
+import java.rmi.RMISecurityManager;
+import java.rmi.Remote;
+import java.rmi.server.RMIClassLoader;
+import java.util.Properties;
 
+@SuppressWarnings("deprecation")
+public class ChatServerStarter {
+	public ChatServerStarter() {
+		// Crea e instala el gestor de seguridad
+		if (System.getSecurityManager() == null) {
+			System.setSecurityManager(new RMISecurityManager());
+		}
+		try {
+			Properties p = System.getProperties();
+			// lee el goodbyes
+			String url = p.getProperty("java.rmi.server.codebase");
+			// Cargador de clases dinámico ...
+			Class<?> serverclass = RMIClassLoader.loadClass(url, "es.ubu.lsi.server.ChatServerImpl");
+			Naming.rebind("/ChatServerImpl", (Remote) serverclass.newInstance());
+			System.out.println("Servidor registrado...");
+		} catch (Exception e) {
+			System.err.println("Excepcion recogida en starter del server: " + e.toString());
+		}
+	}
 }
